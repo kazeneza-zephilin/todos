86ab3dfb82b30574b2dff460332ee24f689f1ff5
kazeneza zephilin <caz@kazenezas-MacBook-Pro.local>
2024-12-14 21:06:31 +0200

    todos initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import "./styles.css";
-import { loadHomePage } from "./home";
-import { loadMenu } from "./menu";
-import { loadAboutPage } from "./about";
-
-loadHomePage() //initial home page rendering
-
-//attaching click handle to the button
-const homeBtn = document.querySelector("#home");
-const menuBtn = document.querySelector("#menu");
-const aboutBtn = document.querySelector("#about");
-
-homeBtn.addEventListener("click", loadHomePage);
-menuBtn.addEventListener("click", loadMenu);
-aboutBtn.addEventListener("click", loadAboutPage);
